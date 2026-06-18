ffef10ac6c06df0c5f6a126931f7b473a24aefd2
mourirdamour <emiialfa08@gmail.com>
2026-06-18 01:37:16 -0500

    fix: restringe la salida de ECS solo a los puertos requeridos

--- iac/modules/security_groups/main.tf
+++ iac/modules/security_groups/main.tf
@@ -46,11 +46,19 @@ resource "aws_security_group" "ecs" {
   }
 
   egress {
-    description = "Allow outbound (ECR pull, Cognito, Secrets, Aurora)"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    description = "HTTPS to AWS services (ECR, Cognito, Secrets Manager)"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description     = "Aurora PostgreSQL"
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.aurora.id]
   }
 
   tags = { Name = "${var.name_prefix}-ecs-sg" }
